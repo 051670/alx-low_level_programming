5ccf464e3fc34fa918588e240f21d953b834d3e3
051670 <khiwot54@gmail.com>
2022-12-12 02:06:53 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main block
+*Description:print ass single digit number of base 10
+*Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '0'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
+	return (0);
+}
+
