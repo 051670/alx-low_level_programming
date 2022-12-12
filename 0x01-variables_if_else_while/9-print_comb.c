d2673ddd0bbbed4db35a597fd92d37b8985d8efb
051670 <khiwot54@gmail.com>
2022-12-12 02:38:36 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main block
+*Description: get random number and check its
+*Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0;a < 100; a++)
+	{
+		putchar(a + '0');
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
